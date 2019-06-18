e065f56a1dc49f547771a80d1abec1ae77ed86cd
Lisu <38836111+barteklis@users.noreply.github.com>
2019-06-18 23:06:26 +0200

    2.0

--- src/SCP.js
+++ src/SCP.js
@@ -1,33 +1,38 @@
-var StartColor = getComputedStyle(document.body).getPropertyValue('--ScpColor');
-var EndColor = localStorage.getItem("SCPcolor");
-var StyleVar = document.documentElement.style;
+const StartColor = getComputedStyle(document.body).getPropertyValue('--ScpColor');
+let FinalColor = localStorage.getItem("SCPcolor");
+const CSSVar = document.documentElement.style;
 if (typeof (Storage) !== "undefined") {
-    if(EndColor == null){
+    if (FinalColor !== null) {
+        CSSVar.setProperty("--ScpColor", FinalColor);
+    } else {
         localStorage.setItem("SCPcolor", StartColor);
     }
-    
-    if(document.getElementById("scpReset")!= null){
-        document.getElementById("scpReset").onclick = function() {SCPreset()};
-    }
-    if(document.getElementById("scpChange") != null){
-        document.getElementById("scpChange").onchange = function() {SCPchange()};
-        function SCPchange() {
-            var InputColor = document.getElementById("scpChange").value;
-            localStorage.setItem("SCPcolor", InputColor);
-            StyleVar.setProperty("--ScpColor", InputColor);
+
+    document.addEventListener("change", (event) => {
+        let SPCpick = event.target.classList.contains("SPCpick");
+        if (SPCpick) {
+            localStorage.setItem("SCPcolor", event.target.value);
+            CSSVar.setProperty("--ScpColor", event.target.value);
         }
-    }
-    function SCPselect(scpSelect) {
-        localStorage.setItem("SCPcolor", scpSelect);
-        StyleVar.setProperty("--ScpColor", scpSelect);
-    }
-    function SCPreset(){
-        localStorage.removeItem("SCPcolor");
-        StyleVar.setProperty("--ScpColor", StartColor);
-    } 
-    StyleVar.setProperty("--ScpColor", EndColor);
-    } 
+    })
+
+    document.addEventListener("click", (event) => {
+        let SPCreset = event.target.classList.contains("SPCreset");
+        if (SPCreset) {
+            localStorage.removeItem("SCPcolor");
+            CSSVar.setProperty("--ScpColor", StartColor);
+        }
+    })
+
+    document.addEventListener("click", (event) => {
+        let SPCchange = event.target.classList.contains("SPCchange");
+        if (SPCchange) {
+            console.log();
+            localStorage.setItem("SCPcolor", event.target.dataset.spc);
+            CSSVar.setProperty("--ScpColor", event.target.dataset.spc);
+        }
+    })
+}
 else {
-    StyleVar.setProperty("--ScpColor", StartColor);
-    EndColor == StartColor;
+    CSSVar.setProperty("--ScpColor", StartColor);
 }
